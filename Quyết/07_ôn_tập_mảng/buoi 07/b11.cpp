8ac4aab2c107d85417a9dedc4910d506d3abc9ce
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-07 10:13:58 +0700

    Quyết/07_ôn_tập_mảng/buoi 07/b11.cpp

---
+++ Quyết/07_ôn_tập_mảng/buoi 07/b11.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    int a[1000];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    sort(a, a + n);
+    
+    for (int i = n - 1; i >= 0; i--)
+        cout << a[i] << " ";
+    
+}
+
+
