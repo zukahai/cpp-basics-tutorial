b4084f5ffafea20a85f79a103edce36209389552
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-07 10:11:02 +0700

    Quyết/05_ôn_tập_vòng_lặp_if_else/Buoi_05/b12.cpp

---
+++ Quyết/05_ôn_tập_vòng_lặp_if_else/Buoi_05/b12.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    int dem = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        if (i % 2 != 0)
+        {
+            dem += 1;
+        }
+    cout << dem;
+}
