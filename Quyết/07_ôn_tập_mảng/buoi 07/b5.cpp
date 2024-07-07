947ee979506c1503f82eac54c31ffff058ac6e21
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-07 10:13:26 +0700

    Quyết/07_ôn_tập_mảng/buoi 07/b5.cpp

---
+++ Quyết/07_ôn_tập_mảng/buoi 07/b5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    int a[1000];
+    int dem = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        if (a[i] % 2 != 0)
+        {
+            dem += 1;
+        }
+    cout << dem * (dem - 1)/2;
+}
+
+// 1 3 5 7
+/**
+dem * (dem - 1) / 2;
+ */
+
