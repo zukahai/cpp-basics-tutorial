03dd7934fecd95fa088e17aeabd9e41786d6b6ed
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-07 10:10:17 +0700

    Quyết/05_ôn_tập_vòng_lặp_if_else/Buoi_05/b5.cpp

---
+++ Quyết/05_ôn_tập_vòng_lặp_if_else/Buoi_05/b5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n >= 10 && n <= 99)
+    {
+        cout << n << " la so co 2 chu so";
+    }
+    else
+    {
+        cout << n << " khong phai la so co 2 chu so";
+    }
+}
