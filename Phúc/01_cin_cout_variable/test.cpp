f58306705996a1944303604bf2f74521fe12ee43
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-13 15:56:52 +0700

    Phúc/01_cin_cout_variable/test.cppPhúc/b1.cpp

--- Phúc/01_cin_cout_variable/test.cpp
+++ Phúc/01_cin_cout_variable/test.cpp
@@ -3,8 +3,33 @@
 using namespace std;
 
 int main() {
-	short a = 33000;
-	cout << a;
+	
+	int a = 2;
+	switch (a) {
+		case 1: {
+			cout << "Thuc hien khoi lenh 1";
+			break;
+		}
+		case 2: {
+			cout << "Thuc hien khoi lenh 2";
+			break;
+		}
+		case 3: {
+			cout << "Thuc hien khoi lenh 3";
+			break;
+		}
+		default: {
+			"Khoi lenh mac dinh";
+			break;
+		}
+	}
+	
+	
+	if (a > 10 || a < 5) {
+		// dieu kien dung
+	}
+	
+	
 }
 
 
