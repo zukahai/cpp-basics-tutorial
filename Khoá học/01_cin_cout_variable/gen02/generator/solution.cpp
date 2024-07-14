a9fcbd20ee72c86a8bae5433d568df59095bdadd
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-14 18:28:23 +0700

    feat: Add C++ code for automatic grading system

---
+++ Khoá học/01_cin_cout_variable/gen02/generator/solution.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    int a;
+    cin >> a;
+    cout << "a = " << a;
+}
