4a2f37a732b3891f99df4a89eb29fa3cf24a291f
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-14 20:16:14 +0700

    feat: Add code for generating input and output files for age exercise

---
+++ Khoá học/01_cin_cout_variable/gen05/generator/solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    int a, b;
+    cin >> a >> b;
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " / " << b << " = " << a / b;
+}
