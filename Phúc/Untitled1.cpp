59a16dfe9c033cb6f8f735afe7b3560146b48367
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-14 18:02:33 +0700

    Phúc/Untitled1.cppPhúc/01_cin_cout_variable/ganBien.cpp

---
+++ Phúc/Untitled1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); // T?t d?ng b? hóa gi?a C++ stream và C stream
+    cin.tie(NULL); // T?t vi?c ràng bu?c cin v?i cout
+    cout.tie(NULL); // T?t vi?c ràng bu?c cout v?i cin
+    for (int i = 1; i <= 3; i++) {
+    	int a;
+    	cin >> a;
+    	cout << a << endl;
+	}
+}
