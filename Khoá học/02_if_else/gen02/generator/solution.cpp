32fc36d0a2a0557df0c2add48973f6f3527fa969
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-16 22:01:40 +0700

    feat: Update arithmetic operations in ex5.md

---
+++ Khoá học/02_if_else/gen02/generator/solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    int n;
+    cin >> n;
+    if (n > 0) 
+        cout << "YES";
+    else
+        cout << "NO";
+}
