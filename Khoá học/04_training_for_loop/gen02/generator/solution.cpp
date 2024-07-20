b335e9639bc787814da7ed286d74455575f33547
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-20 22:54:52 +0700

    feat: Generate input and output files for problem 10

---
+++ Khoá học/04_training_for_loop/gen02/generator/solution.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    int n = 2;
+    for (int i = 1; i <= 10; i++)
+        cout << "2 x " << i << " = " << 2 * i << endl;
+}
