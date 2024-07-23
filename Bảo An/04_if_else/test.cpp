167e54636c5d6202276de4bcd9153ec16221244e
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-23 13:46:28 +0700

    feat: Add if-else logic to handle different values of n

---
+++ Bảo An/04_if_else/test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	
+	if (n == 10) {
+		cout << "10 bang 10";
+	} else {
+		cout << n;
+		cout << " khong bang 10";
+	}
+}
+
