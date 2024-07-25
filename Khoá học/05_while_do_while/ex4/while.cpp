bc97c580bd44692ff212a249fb125dafe372968d
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-25 09:35:48 +0700

    feat: Add for loop to print numbers from 1 to 10

---
+++ Khoá học/05_while_do_while/ex4/while.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	
+	int n;
+	do {
+		cin >> n;
+	} while(n != 0);
+	
+	cout << "Chuong trinh ket thuc.";
+	
+
+	// Cho nguoi dung nhap vao mot so nguyen
+	// den khi nao nhap so 0 thi dung
+	/*
+	4
+	3
+	-6
+	0
+	n != 0
+	
+	
+	*/
+}
