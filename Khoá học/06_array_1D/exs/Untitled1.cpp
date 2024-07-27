239a866272b6626eac708a3127f12b59dccbf323
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-07-27 09:14:28 +0700

    Refactor code to use a fixed-size array instead of a dynamic one

--- Khoá học/06_array_1D/exs/Untitled1.cpp
+++ Khoá học/06_array_1D/exs/Untitled1.cpp
@@ -1,18 +1,12 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-int a[10000000];
-
-int main() {
-	int n; // so phan tu trong mang
-	cin >> n;
-	// N so, bat dau tu 0 -> n - 1
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-	
-	cout << a[n - 1];
-	
-	
+int a[3];
+int main (){
+    a[0]=1;
+    a[1]=6;
+    a[2]=7;
+    for (int i=0;i < 3;i++)
+        cout << a[i] << endl;
 }
+
