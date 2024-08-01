e610dceedbe5a4f60f066f1b2e17c6a5aebfafd8
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-01 14:21:21 +0700

    Phúc/09_ký_tự_và_xâu/gen01/generator/solution.cpp

---
+++ Phúc/09_ký_tự_và_xâu/gen01/generator/solution.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+    char c;
+    cin >> c;
+    cout << c << endl << (int)c;
+}
