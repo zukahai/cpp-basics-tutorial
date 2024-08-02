1f512ac26fa663c336e17ed8221876d712400f5e
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-02 14:44:02 +0700

    add file commit_and_push.sh

--- commit_and_push.sh
+++ commit_and_push.sh
@@ -1,21 +1,23 @@
-#!/bin/bash
-
 # Lấy danh sách các tệp tin đã thay đổi nhưng chưa được staged
-unstaged_files=$(git diff --name-only)
+$unstaged_files = git diff --name-only
 
 # Lấy danh sách các tệp tin đã staged
-staged_files=$(git diff --cached --name-only)
+$staged_files = git diff --cached --name-only
 
 # Kết hợp danh sách các tệp tin
-all_files="$unstaged_files $staged_files"
+$all_files = $unstaged_files + "`n" + $staged_files
+$all_files = $all_files -split "`n"
 
-for file in $all_files; do
-    # Thêm từng tệp vào staging area
-    git add "$file"
-    
-    # Commit từng tệp với thông điệp commit cụ thể
-    git commit -m "add file $file"
-done
+foreach ($file in $all_files) {
+    if ($file -ne "") {
+        # Thêm từng tệp vào staging area
+        git add $file
+        
+        # Commit từng tệp với thông điệp commit cụ thể
+        git commit -m "add file $file"
+        git push
+    }
+}
 
 # Đẩy các commit lên remote repository
-git push
+
