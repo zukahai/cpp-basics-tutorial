e6763f62502612ec54bedc7fb3c972e8ba30aec9
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-02 15:47:44 +0700

    Update generator test files for functional course

--- Khoá học/09_functional/genTC/generator/solution.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-    ifstream cin("input.txt");
-    ofstream cout("output.txt");
-    cout << "";
-}
--- Khoá học/genTC/generator/solution.cpp
+++ Khoá học/genTC/generator/solution.cpp
@@ -5,4 +5,6 @@ int main()
 {
     ifstream cin("input.txt");
     ofstream cout("output.txt");
+
+    
 }
