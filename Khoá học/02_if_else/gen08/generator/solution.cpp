7541cc5a960a8d3f7955268e41570ac6a962ae2d
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-07 15:30:25 +0700

    Resolved bugs in 02_if_else/gen08/generator/solution.cpp

---
+++ Khoá học/02_if_else/gen08/generator/solution.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    int a, b;
+    cin >> a >> b;
+    if (a == b) {
+        cout << a << " = " << b;
+    } else {
+        if (a > b) {
+            cout << a << " > " << b;
+        } else {
+            cout << a << " < " << b;
+        }
+    }
+    
+}
