4d76645c5fb281f59766d2a0e6530d958d295fb6
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-09 18:54:22 +0700

    Enhanced user experience for 03_for_loop/gen14/generator/generator.h

---
+++ Khoá học/03_for_loop/gen14/generator/generator.h
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include "../lib/library.h"
+using namespace std;
+
+void gen(int iTest, int testnum, string target_file)
+{
+    ofstream cout(target_file);
+    
+    // Cout ra những input cần thiết
+    cout << random((iTest - 1) * 20, iTest * 20) << endl;
+}
