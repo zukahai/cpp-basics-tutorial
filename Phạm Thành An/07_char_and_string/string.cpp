2cb151f0ff5af3c4a9c6fcea1a94ce7ebf7d9e90
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-17 11:16:53 +0700

    Refactor code to add functionality for converting a number to a string and vice versa

---
+++ Phạm Thành An/07_char_and_string/string.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // to_string() converts a number to a string
+    // stoi() converts a string to an integer
+    // stod() converts a string to a double
+    // stof() converts a string to a float
+    // stoll() converts a string to a long long
+}
