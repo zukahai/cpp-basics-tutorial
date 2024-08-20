ffcdc49055b956ee6072cd8c35d6fcbb702c00ed
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-20 20:00:05 +0700

    feat: Add file.cpp with input/output functionality

---
+++ Phạm Thành An/09_file_iostream/file.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ifstream fi("an.inp");
+    ofstream fo("an.out");
+    int a, b;
+    fi >> a >> b;
+    fo << a + b;
+    return 0;
+}
