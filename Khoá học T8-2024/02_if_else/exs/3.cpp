9f71192b66affa5ba40fa244d18bad96f001691f
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-29 20:59:38 +0700

    refactor: Improve if-else code for exercises

---
+++ Khoá học T8-2024/02_if_else/exs/3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	
+	if (n % 2 == 0) { // Neu N chia 2 du 0 thi la so chan
+		cout << "Even";
+	} else {
+		cout << "Odd";
+	}
+}
+
+
---
+++ Khoá học T8-2024/02_if_else/exs/5.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	
+	if (n >= 100 && n < 1000) {
+		cout << "YES";
+	} else {
+		cout << "NO";
+	}
+	
+}
+
+
