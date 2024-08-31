b524310a0bbcc1028f66e49962f3580659293ff0
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-08-31 22:16:54 +0700

    refactor: Improve code for calculating the sum of numbers in a range

--- Khoá học T8-2024/04_training_loop/test/main.cpp
+++ Khoá học T8-2024/04_training_loop/test/main.cpp
@@ -5,9 +5,18 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
+	int tong = 0;
 	for (int i = 1; i <= n; i++) {
-		cout << i << endl;
+		tong += i; // tong = tong + i
 	}
+	cout << tong;
+
+//	int tong = 1 + 2 + 3 + 4 + ... + n;
+//	int tong = 0;
+//	tong = tong + 1; // 1
+//	tong = tong + 2; // 3
+//	tong = tong + 3; // 3
+//	tong = tong + 4;
 	
 }
 
