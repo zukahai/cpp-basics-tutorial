98bd32ccee0b3a84c1e504a5855972973fb05aac
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-09-21 19:47:39 +0700

    Refactor and improve structure of 05_for_loop_(03)/readme.md

---
+++ Khoá học T8-2024/09_char_and_string/test/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	char ch;
+	cin >> ch;
+	if (ch >= '0' && ch <= '9') {
+		cout << "YES";
+	} else {
+		cout << "NO";
+	}
+}
---
+++ Nhóm học C++ An Ngân Hằng/01_if_else/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    if (10 > 30) {
+        if (2 > 0) {
+            cout << "A";
+        } else {
+            cout << "B";
+        }
+    } else {
+        if (40 < 10) {
+            cout << "C";
+        } else {
+            if (10 > 60) {
+                cout << "D";
+            }
+        }
+    }
+}
