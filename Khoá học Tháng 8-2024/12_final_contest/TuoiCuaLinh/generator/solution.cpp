8d6e3a9f9d7e130395c39858346c7b5708632dd0
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-09-30 19:22:52 +0700

    Enhanced compatibility with TuoiCuaLinh/generator/solution.cpp

---
+++ Khoá học Tháng 8-2024/12_final_contest/TuoiCuaLinh/generator/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ifstream cin("input.txt");
+    ofstream cout("output.txt");
+
+    //Tuoi cua Linh la 19
+
+    int ns;
+    cin >> ns;
+    cout << "Tuoi cua Linh la " << 2024 - ns;
+
+    
+}
