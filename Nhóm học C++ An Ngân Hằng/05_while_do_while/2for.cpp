a9a0b65a376047cfd4bf0a0b6c553fbdef486187
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-10-10 10:35:25 +0700

    Refactor loop logic in 2for.cpp and tamGiacRong.cpp

---
+++ Nhóm học C++ An Ngân Hằng/05_while_do_while/2for.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) { // i: số dòng
+        if (i == 1 || i == n) {
+            for (int j = 1; j <= n; j++) {
+                cout << "*";
+            }
+        } else {
+            cout << "*";
+            for (int j = 1; j <= n - 2; j++) {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+  
+}
