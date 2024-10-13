f8aa7b232bd191dd9c49bee8e8fb40d91257c355
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-10-13 17:42:40 +0700

    Refactor char.cpp and string.cpp files

---
+++ Nguyễn Hải Đăng 23-10-2013/05_string_char/char.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int ch;
+    ch = 'm';
+    cout << (char)(ch - 32);
+
+}
---
+++ Nguyễn Hải Đăng 23-10-2013/05_string_char/string.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+    for (int i = s.length() - 1; i >= 0; i--) {
+        cout << s[i];
+    }
+
+}
