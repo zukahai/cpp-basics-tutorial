7d0cfb7561a20e958271e8340b521bbe556540bd
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-10-23 22:25:29 +0700

    Refactor if-else logic in Main.cpp and remove unused file a.cpp

---
+++ Khoá học T10-2024/02_if_else/LiThuyet/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int a = 8;
+	int b = 1;
+	if (a > 5) {
+		if (b < 4) {
+			cout << "A";
+		} else {
+			cout << "B";
+		}
+	} else {
+		if (b <= 2) {
+			cout << "C";
+		} else {
+			cout << "D";
+		}
+	}
+	
+	
+}
