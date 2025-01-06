294faabab7e194788d98174f91e9c462d8539f3a
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2025-01-06 13:09:11 +0700

    Add new C++ programs demonstrating nested and exponential for loops

---
+++ Nguyễn Ngọc Diễm Phúc/04_traning_for_loop/2for.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 3; j++) {
+            cout << i << " " << j << endl;
+        }
+        cout << endl;
+    }
+}
---
+++ Nguyễn Ngọc Diễm Phúc/04_traning_for_loop/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 20; i *= 2) {
+        cout << i << endl;
+    }
+}
